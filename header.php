dc1f9526e2f49e2dda3c37d61c3764c3ef1ff503
FernE97 <ferne97@gmail.com>
2012-02-20 23:08:46 -0800

    Added dynamic_body class function. Script updates, code cleanup.

--- header.php
+++ header.php
@@ -13,45 +13,48 @@
 <!--[if IE 9 ]> <html <?php language_attributes(); ?> class="no-js ie9"> <![endif]-->
 <!--[if (gt IE 9)|!(IE)]><!--> <html <?php language_attributes(); ?> class="no-js"> <!--<![endif]-->
 <head>
-	<meta charset="<?php bloginfo('charset'); ?>">
-	<meta name="viewport" content="width=device-width; initial-scale=1.0; maximum-scale=1.0;">
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta name="author" content="Eric Fernandez | www.efdezigns.com">
+	<!-- Enable if site is optimized for mobile devices -->
+	<!-- <meta name="viewport" content="width=device-width, initial-scale=1.0"> -->
 	
-	<title><?php wp_title('|', true, 'right'); bloginfo('name'); ?></title>
+	<title><?php wp_title( '|', true, 'right' ); bloginfo( 'name' ); ?></title>
 	
 	<!-- Place favicon.ico and apple-touch-icon.png in the root of the domain -->
-
+	
 	<!-- Versioning enabled for caching -->
-	<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>?v=1.0">
+	<link rel="stylesheet" href="<?php bloginfo( 'stylesheet_url' ); ?>?v=1.2">
 	
 	<?php // JavaScript added through functions.php to avoid conflicts ?>
 	
 	<!-- Enables advanced css selectors in IE, must be used with a JavaScript library (jQuery Enabled in functions.php) -->
 	<!--[if lt IE 9]>
-		<script src="<?php bloginfo('template_url'); ?>/lib/js/selectivizr.js?v=1.0.1"></script>
+		<script src="<?php bloginfo( 'template_url' ); ?>/lib/js/selectivizr.js?v=1.0.2"></script>
 	<![endif]-->
-
-	 <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
-	 
+	
+	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+	
 	<?php wp_head(); ?>
 </head>
 
-<body <?php if (is_front_page()) { ?> id="home"<?php } elseif (is_home() || is_single() || is_archive()) { ?> id="blog" <?php } else { ?> id="interior" class="<?php $post_data = get_post($post->post_parent); echo $slug = $post_data->post_name; ?>"<?php } ?>>
+<body <?php dynamic_body(); ?>>
 
 <div id="container">
 	<header role="banner">
 		<nav role="navigation">
-		<?php if ( has_nav_menu('primary') ) {
+		<?php if ( has_nav_menu( 'primary' ) ) {
 			wp_nav_menu( array (
-				'theme_location'    => 'primary',
-				'container'         => '',
-				'menu_id'           => 'nav-main',
-				'depth'             => 0, // set to 1 to disable dropdowns
-				'fallback_cb'       => false
+				'theme_location' => 'primary',
+				'container'      => '',
+				'menu_id'        => 'nav-main',
+				'depth'          => 0, // set to 1 to disable dropdowns
+				'fallback_cb'    => false
 			));
 		} else { ?>
 			<ul id="nav-main" class="menu">
-				<?php wp_list_pages('title_li='); ?>
+				<?php wp_list_pages( 'title_li=' ); ?>
 			</ul>
-		<?php } ?>
+			<?php
+		} ?>
 		</nav>
 	</header>
