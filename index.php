d260cd0913cd117eeec0d71bfc3cf92b01cb332d
FernE97 <ferne97@gmail.com>
2012-02-20 23:31:54 -0800

    code cleanup

--- index.php
+++ index.php
@@ -9,12 +9,12 @@ get_header(); ?>
 
 	<div id="content" role="main">
 
-		<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+		<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 		
-		<article class="post" id="post-<?php the_ID(); ?>">
+		<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 			<header>
 				<h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-				<time datetime="<?php the_time('Y-m-d') ?>" pubdate><?php the_time('F j, Y') ?></time>
+				<time datetime="<?php the_time( 'Y-m-d' ) ?>" pubdate><?php the_time( 'F j, Y' ) ?></time>
 			</header>
 			<?php the_excerpt(); ?>
 		</article>
@@ -22,8 +22,8 @@ get_header(); ?>
 		<?php endwhile; endif; ?>
 		
 		<div class="page-nav">
-			<div class="alignleft"><?php next_posts_link('&laquo; Older Entries') ?></div>
-			<div class="alignright"><?php previous_posts_link('Newer Entries &raquo;') ?></div>
+			<div class="alignleft"><?php next_posts_link( '&laquo; Older Entries' ) ?></div>
+			<div class="alignright"><?php previous_posts_link( 'Newer Entries &raquo;' ) ?></div>
 		</div>
 
 	</div><!-- end content -->
