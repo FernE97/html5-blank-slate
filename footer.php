e3865fa459b83cd50516186d712a47c709cfbc7d
Eric Fernandez <ferne97@gmail.com>
2012-08-16 00:05:13 -0700

    updated google analytics code

--- footer.php
+++ footer.php
@@ -42,10 +42,10 @@
 
 <?php wp_footer(); ?>
 
-<!-- asynchronous google analytics -->
+<!-- Google Analytics -->
 <script>
 	var _gaq=[['_setAccount','UA-XXXXX-X'],['_trackPageview']];
-	(function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];g.async=1;
+	(function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
 	g.src=('https:'==location.protocol?'//ssl':'//www')+'.google-analytics.com/ga.js';
 	s.parentNode.insertBefore(g,s)}(document,'script'));
 </script>
