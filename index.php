8dda70d6ad48285df96e5668d35b68b105c769f1
Eric Fernandez <ferne97@gmail.com>
2012-10-23 22:53:28 -0700

    removed repetitive article post id

--- index.php
+++ index.php
@@ -10,7 +10,7 @@ get_header(); ?>
 
 		<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 		
-		<article id="post-<?php the_ID(); ?>" <?php post_class( 'group' ); ?> role="article">
+		<article <?php post_class( 'group' ); ?> role="article">
 			<header>
 				<h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
 				<time datetime="<?php the_time( 'Y-m-d' ) ?>" pubdate><?php the_time( 'F j, Y' ) ?></time>
--- single.php
+++ single.php
@@ -10,12 +10,14 @@ get_header(); ?>
 
 		<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 		
-		<article id="post-<?php the_ID(); ?>" <?php post_class( 'group' ); ?> role="article">
+		<article <?php post_class( 'group' ); ?> role="article">
 			<header>
 				<h1><?php the_title(); ?></h1>
 				<time datetime="<?php the_time( 'Y-m-d' ) ?>" pubdate><?php the_time( 'F j, Y' ) ?></time>
 			</header>
+
 			<?php the_content(); ?>
+			
 			<section>
 				<?php comment_form(); ?>
 			</section>
