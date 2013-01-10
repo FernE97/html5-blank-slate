d150234aafb300e6d8ed9ede57f9fe3d6c658f1f
Eric Fernandez <ferne97@gmail.com>
2013-01-10 01:13:31 -0800

    Update modernizr to 2.6.2

--- functions.php
+++ functions.php
@@ -88,7 +88,7 @@ add_action( 'init', 'h5bs_remove_junk' );
 function h5bs_enqueue_scripts() {
 	wp_deregister_script( 'jquery' ); // Load Jquery from Google CDN instead
 	wp_register_script( 'jquery', 'https://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js', array(), '1.8.0', true );
-	wp_register_script( 'modernizr', get_template_directory_uri() . '/lib/js/modernizr.js', array(), '2.6.1', false );
+	wp_register_script( 'modernizr', get_template_directory_uri() . '/lib/js/modernizr.js', array(), '2.6.2', false );
 	// wp_register_script( 'plugins', get_template_directory_uri() . '/lib/js/plugins.js', array( 'jquery' ), '1.0', true );
 	wp_register_script( 'main-js', get_template_directory_uri() . '/lib/js/main.js', array( 'jquery' ), '1.0', true );
 
