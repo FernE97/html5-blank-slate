785b480aa1281474d93b18d323b8dd0025a84b79
Eric Fernandez <ferne97@gmail.com>
2015-04-28 14:15:27 -0700

    cleanup analytics conditional

--- footer.php
+++ footer.php
@@ -40,24 +40,20 @@
 <?php wp_footer(); ?>
 
 <?php
-if ( ! current_user_can( 'edit_pages' ) ) : // don't track admins or editors
-    $analytics_id = '';
+// don't track admins or editors and google analytics option is filled in
+if ( ! current_user_can( 'edit_pages' ) && get_option( 'client_google_analytics' ) ) :
+    $analytics_id = get_option( 'client_google_analytics' ); ?>
 
-    if ( get_option( 'client_google_analytics' ) ) {
-        $analytics_id = get_option( 'client_google_analytics' ); ?>
-
-        <!-- Google Universal Analytics -->
-        <script>
-            (function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
-            function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
-            e=o.createElement(i);r=o.getElementsByTagName(i)[0];
-            e.src='//www.google-analytics.com/analytics.js';
-            r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
-            ga('create','<?php echo $analytics_id; ?>','auto');ga('send','pageview');
-        </script>
-        <?php
-    }
- endif; ?>
+    <!-- Google Universal Analytics -->
+    <script>
+        (function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
+        function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
+        e=o.createElement(i);r=o.getElementsByTagName(i)[0];
+        e.src='//www.google-analytics.com/analytics.js';
+        r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
+        ga('create','<?php echo $analytics_id; ?>','auto');ga('send','pageview');
+    </script>
+<?php endif; ?>
 
 </body>
 </html>
