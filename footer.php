77b1bac98b305492a888633763944e7a5d6fb76b
Eric Fernandez <ferne97@gmail.com>
2016-04-07 10:56:38 -0700

    update google analytics / remove focus outline per Ben's request

--- footer.php
+++ footer.php
@@ -24,12 +24,11 @@ if ( ! current_user_can( 'edit_pages' ) && get_option( 'client_google_analytics'
 
     <!-- Google Universal Analytics -->
     <script>
-        (function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
-        function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
-        e=o.createElement(i);r=o.getElementsByTagName(i)[0];
-        e.src='//www.google-analytics.com/analytics.js';
-        r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
-        ga('create','<?php echo $analytics_id; ?>','auto');ga('send','pageview');
+        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+        })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+        ga('create', '<?php echo $analytics_id; ?>', 'auto');ga('send', 'pageview');
     </script>
 <?php endif; ?>
 
