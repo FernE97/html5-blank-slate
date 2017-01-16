36e405f828fc12ae6a068ea2122d7755831f4bd4
Guy Lyons <guy@hopsie.com>
2017-01-16 11:56:48 -0500

    only inject browsersync if on localhost

--- functions.php
+++ functions.php
@@ -289,16 +289,22 @@ function h5bs_img_caption_shortcode_filter($val, $attr, $content = null) {
 
 // BrowserSync script
 function browser_sync() {
+    $whitelist = array(
+        '127.0.0.1',
+        '::1'
+    );
+if(in_array($_SERVER['REMOTE_ADDR'], $whitelist)){
     echo "
     <!-- BrowserSync -->
     <script id=\"__bs_script__\">//<![CDATA[
         document.write(\"<script async src='http://HOST:3000/browser-sync/browser-sync-client.js?v=2.18.2'><\/script>\".replace(\"HOST\", location.hostname));
-    //]]></script>
-    ";
+        //]]></script>
+        ";
+    }
 }
-// FIXME: comment this out before shipping
 add_action( 'wp_footer', 'browser_sync' );
 
+// Helper function for printing clean var dumps
 function dump( $var ) {
     echo "<pre>";
     print_r( $var );
