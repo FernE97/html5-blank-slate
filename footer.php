6204a06b6d9c7faea1145692098711108f2fbbf9
Guy Lyons <guy@hopsie.com>
2017-02-20 16:29:26 -0500

    move ga into head

--- footer.php
+++ footer.php
@@ -16,21 +16,5 @@
 </footer>
 
 <?php wp_footer(); ?>
-
-<?php
-// don't track admins or editors and google analytics option is filled in
-if ( ! current_user_can( 'edit_pages' ) && get_option( 'client_google_analytics' ) ) :
-    $analytics_id = get_option( 'client_google_analytics' ); ?>
-
-    <!-- Google Universal Analytics -->
-    <script>
-        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-        })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-        ga('create', '<?php echo $analytics_id; ?>', 'auto');ga('send', 'pageview');
-    </script>
-<?php endif; ?>
-
 </body>
 </html>
--- header.php
+++ header.php
@@ -10,12 +10,12 @@
     <meta charset="<?php bloginfo( 'charset' ); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
-
+    <?php get_template_part( 'parts/google-analytics' ); ?>
     <?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
 
-<header class="site-header" role="banner">
-    <?php get_template_part( 'parts/nav' ); ?>
-</header>
+    <header class="site-header" role="banner">
+        <?php get_template_part( 'parts/nav' ); ?>
+    </header>
