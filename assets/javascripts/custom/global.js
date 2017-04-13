d0f22ef574f5ef87e472b153188d8a350e9866f5
Guy Lyons <guy@hopsie.com>
2017-04-13 15:13:31 -0400

    keep bare

--- assets/javascripts/custom/global.js
+++ assets/javascripts/custom/global.js
@@ -1,14 +1,8 @@
-// Custom global scripts
 jQuery(document).ready(function($) {
     'use strict';
 
     // Foundation Init
     $(document).foundation();
-
-    // Mobile nav toggle
-    $('#nav-mobile-toggle').on('click', function() {
-        $('#nav-mobile').slideToggle();
-    });
 
     // Sanity check
     console.log("Global.js loaded!");
