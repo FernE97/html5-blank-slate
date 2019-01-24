7a6555868e3e0d4ca49bbadf093fff2537d5167d
Alex Brown <br0wnie@users.noreply.github.com>
2019-01-24 16:14:31 -0500

    Donation form JS

--- src/js/global.js
+++ src/js/global.js
@@ -1,24 +1,110 @@
 import Glide from '@glidejs/glide'
-//import glide from 'glide-js'
+
+// Custom global jQuery scripts
+jQuery(document).ready(function ($) {
+  'use strict';
+
+  // Foundation Init
+  $(document).foundation();
+
+  // Mobile nav toggle
+  $('#nav-mobile-toggle').on('click', function () {
+    $('#nav-mobile').slideToggle();
+  });
 
 
+  // -------------------- DONATION FORM ------------------- //
+  let donateLabel = document.querySelectorAll('.donateForm__radioLabel'),
+    i;
 
+  for (i = 0; i < donateLabel.length; ++i) {
+    let el = donateLabel[i];
+    el.addEventListener('click', function () {
+      let formLabel;
+      if (document.querySelector('.donateForm__radioLabel--active')) {
+        formLabel = document.querySelector('.donateForm__radioLabel--active');
+        formLabel.classList.remove('donateForm__radioLabel--active');
+      }
+      el.classList.add('donateForm__radioLabel--active');
+    })
+  }
 
-// Custom global scripts
-jQuery(document).ready(function($) {
-    'use strict';
+  var buttonAmount = $('.donateForm__input'),
+    customAmount = $('.donateForm__input'),
+    buttonDonate = $('.js--button-donate'),
+    frequencyButton = $('.js--frequency'),
+    urlValue
 
-    // Foundation Init
-    $(document).foundation();
+  // Toggling amount
+  buttonAmount.on('change', function () {
+    $('.donateForm__input').val();
+  });
 
-    // Mobile nav toggle
-    $('#nav-mobile-toggle').on('click', function() {
-        $('#nav-mobile').slideToggle();
-    });
+  // Resetting amounts and setting the custom-amount
+  customAmount.on('change', function () {
+    $('.js--amount').removeClass('active');
+  });
 
+  buttonDonate.on('click', function (e) {
+    e.preventDefault();
+    var amountInput = customAmount.val(),
+      amountButton = $('.donateForm__input').val(),
+      frequency;
+
+    if ($('.donateForm__radioLabel--active').data('interval') == 'monthly') {
+      frequency = '&interval=' + $('.donateForm__radioLabel--active').data('interval')
+    } else {
+      frequency = ''
+    }
+
+    urlValue = e.target.href + '?amount=' + amountButton + frequency
+    if ($.trim(amountInput).match(/^\d+$/) || $.trim(amountButton).match(/^\d+$/))
+      window.location.replace(urlValue);
+  });
 }(jQuery));
 
-function addAttribute(selector, attribute, attributeValue) {
+
+// IE11 Custom Elements Polyfill
+(function () {
+  if (typeof window.CustomEvent === "function") return false;
+
+  function CustomEvent(event, params) {
+    params = params || {
+      bubbles: false,
+      cancelable: false,
+      detail: undefined
+    };
+    var evt = document.createEvent('CustomEvent');
+    evt.initCustomEvent(event, params.bubbles, params.cancelable, params.detail);
+    return evt;
+  }
+  CustomEvent.prototype = window.Event.prototype;
+  window.CustomEvent = CustomEvent;
+})();
+
+// Optimized resize custom event
+(function () {
+  var throttle = function (type, name, obj) {
+    obj = obj || window;
+    var running = false;
+    var func = function () {
+      if (running) {
+        return;
+      }
+      running = true;
+      requestAnimationFrame(function () {
+        obj.dispatchEvent(new CustomEvent(name));
+        running = false;
+      });
+    };
+    obj.addEventListener(type, func);
+  };
+  /* init - you can init any event */
+  throttle("resize", "optimizedResize");
+})();
+
+
+var addAttribute = function (selector, attribute, attributeValue) {
   // If it is a node, such as a variable set to document.getElementById('id');
   if (selector.nodeType === Node.ELEMENT_NODE) {
     selector.setAttribute(attribute, attributeValue);
@@ -37,7 +123,7 @@ function addAttribute(selector, attribute, attributeValue) {
   }
 };
 
-var keepSquare = function(selector, matchAttr) {
+var keepSquare = function (selector, matchAttr) {
   if (typeof selector !== 'undefined') {
     var s = document.querySelectorAll(selector);
     if (s.length > 0) {
@@ -59,18 +145,20 @@ var keepSquare = function(selector, matchAttr) {
 
 // Document load/ready promise, calls 'ready' method when completed
 HTMLDocument.prototype.ready = function () {
-  return new Promise(function(resolve, reject) {
+  return new Promise(function (resolve, reject) {
     if (document.readyState === 'complete') {
       resolve(document);
     } else {
-      document.addEventListener('DOMContentLoaded', function() {
+      document.addEventListener('DOMContentLoaded', function () {
         resolve(document);
       });
     }
   });
 };
 
-document.ready().then(function() {
+
+// Custom global scripts
+document.ready().then(function () {
   var glide = document.querySelectorAll('.glide');
   if (glide.length > 0) {
     for (let i = 0; i < glide.length; i++) {
@@ -93,47 +181,10 @@ document.ready().then(function() {
     }
   }
 
-  keepSquare('.glide__slide--circle');
-  keepSquare('.glide__slide--square');
+  // Initial resize
+  keepSquare('.glide__slide');
 });
 
-
-
-// IE11 Custom Elements Polyfill
-(function () {
-  if ( typeof window.CustomEvent === "function" ) return false;
-  function CustomEvent ( event, params ) {
-    params = params || { bubbles: false, cancelable: false, detail: undefined };
-    var evt = document.createEvent( 'CustomEvent' );
-    evt.initCustomEvent( event, params.bubbles, params.cancelable, params.detail );
-    return evt;
-   }
-  CustomEvent.prototype = window.Event.prototype;
-  window.CustomEvent = CustomEvent;
-})();
-
-// Optimized resize custom event
-(function() {
-  var throttle = function(type, name, obj) {
-    obj = obj || window;
-    var running = false;
-    var func = function() {
-      if (running) { return; }
-      running = true;
-      requestAnimationFrame(function() {
-        obj.dispatchEvent(new CustomEvent(name));
-        running = false;
-      });
-    };
-    obj.addEventListener(type, func);
-  };
-
-  /* init - you can init any event */
-  throttle("resize", "optimizedResize");
-})();
-
-
-window.addEventListener("optimizedResize", function() {
-  keepSquare('.glide__slide--circle');
-  keepSquare('.glide__slide--square');
+window.addEventListener("optimizedResize", function () {
+  keepSquare('.glide__slide');
 });
