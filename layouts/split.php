27c76937d75bc01c51f47bbd7482ec0e75719f23
Alex Brown <br0wnie@users.noreply.github.com>
2019-02-11 14:55:50 -0500

    Fixed field name issue
    In the split section, sometimes the foreground image would not appear with the field name 'image'. Changed the field name to 'fg_image' and it appears to be resolved.

--- layouts/split.php
+++ layouts/split.php
@@ -1,52 +1,52 @@
 <section class="section<?=$count;?> split<?=$count;?> split grid-x">
-  <?php if ( have_rows( "left" ) ): ?>
+  <?php if (have_rows("left")): ?>
   <div class="section<?=$count;?>__left splitLeft splitLeft<?=$count;?> cell small-6">
-    <?php while ( have_rows( "left" ) ): the_row(); ?>
-    <div class="section<?=$count;?>__leftContent splitLeft__content section<?=$count;?>__leftContent--<?= $alignment; ?> splitLeft__content--<?= $alignment; ?> grid-y grid-margin-y">
-    <?php if(get_sub_field("header") || get_sub_field("tagline") || get_sub_field("body")) : ?>
-      <div class="section<?=$count;?>__leftText splitLeft__text cell">
-        <?php if(get_sub_field( "header" ) ): ?><h1><?= get_sub_field( "header" ); ?></h1><?php endif; ?>
-        <?php if(get_sub_field( "tagline" ) ): ?><h2><?= get_sub_field( "tagline" ); ?></h2><?php endif; ?>
-        <?php if(get_sub_field( "body" ) ): ?><p><?= get_sub_field( "body" ); ?></p><?php endif; ?>
+    <?php while (have_rows("left")): the_row();?>
+	    <div class="section<?=$count;?>__leftContent splitLeft__content section<?=$count;?>__leftContent--<?=$alignment;?> splitLeft__content--<?=$alignment;?> grid-y grid-margin-y">
+	    <?php if (get_sub_field("header") || get_sub_field("tagline") || get_sub_field("body")): ?>
+	      <div class="section<?=$count;?>__leftText splitLeft__text cell">
+	        <?php if (get_sub_field("header")): ?><h1><?=get_sub_field("header");?></h1><?php endif;?>
+        <?php if (get_sub_field("tagline")): ?><h2><?=get_sub_field("tagline");?></h2><?php endif;?>
+        <?php if (get_sub_field("body")): ?><p><?=get_sub_field("body");?></p><?php endif;?>
       </div>
-    <?php endif; ?>
-    <?php if ( get_sub_field("image")):?><img class="section<?=$count;?>__leftImage splitLeft__image" src="<?= get_sub_field("image")["url"]; ?>"><?php endif;?>
-    <?php if ( have_rows( "left_buttons" ) ): $btn_count = 1; ?>
-    <div class="section<?=$count;?>__leftButtons splitLeft__buttons splitLeft__buttons--<?= $alignment; ?> cell">
-    <?php while ( have_rows( "left_buttons" ) ): the_row(); ?>
-      <a href="<?php the_sub_field( "link" ); ?>" class="section<?=$count;?>__leftButton section<?=$count;?>__leftButton<?=$btn_count;?> button splitLeft__button splitLeft<?=$count;?>__button<?=$btn_count;?>"><?php the_sub_field( "text" ); ?></a>
-    <?php $btn_count++; endwhile; ?>
-    </div>
-    <?php else: ?>
+    <?php endif;?>
+    <?php if (get_sub_field("fg_image")): ?><img class="section<?=$count;?>__leftImage splitLeft__image" src="<?=get_sub_field("fg_image")["url"];?>"><?php endif;?>
+    <?php if (have_rows("left_buttons")): $btn_count = 1;?>
+	    <div class="section<?=$count;?>__leftButtons splitLeft__buttons splitLeft__buttons--<?=$alignment;?> cell">
+	    <?php while (have_rows("left_buttons")): the_row();?>
+		      <a href="<?php the_sub_field("link");?>" class="section<?=$count;?>__leftButton section<?=$count;?>__leftButton<?=$btn_count;?> button splitLeft__button splitLeft<?=$count;?>__button<?=$btn_count;?>"><?php the_sub_field("text");?></a>
+		    <?php $btn_count++;endwhile;?>
+	    </div>
+	    <?php else: ?>
       <?php // no buttons found ?>
-    <?php endif; ?>
+    <?php endif;?>
     </div>
-    <?php endwhile; ?>
+    <?php endwhile;?>
   </div>
   <?php endif; // End of left content ?>
-  <?php if ( have_rows( "right" ) ): ?>
+  <?php if (have_rows("right")): ?>
   <div class="section<?=$count;?>__right splitRight splitRight<?=$count;?> cell small-6">
-    <?php while ( have_rows( "right" ) ): the_row(); ?>
-    <div class="section<?=$count;?>__rightContent section<?=$count;?>__rightContent--<?= $alignment; ?> splitRight__content splitRight__content--<?= $alignment; ?> grid-y grid-margin-y">
-    <?php if(get_sub_field("header") || get_sub_field("tagline") || get_sub_field("body")) : ?>
-      <div class="section<?=$count;?>__rightText splitRight__text cell">
-        <?php if(get_sub_field( "header" ) ): ?><h1><?= get_sub_field( "header" ); ?></h1><?php endif; ?>
-        <?php if(get_sub_field( "tagline" ) ): ?><h2><?= get_sub_field( "tagline" ); ?></h2><?php endif; ?>
-        <?php if(get_sub_field( "body" ) ): ?><p><?= get_sub_field( "body" ); ?></p><?php endif; ?>
+    <?php while (have_rows("right")): the_row();?>
+	    <div class="section<?=$count;?>__rightContent section<?=$count;?>__rightContent--<?=$alignment;?> splitRight__content splitRight__content--<?=$alignment;?> grid-y grid-margin-y">
+	    <?php if (get_sub_field("header") || get_sub_field("tagline") || get_sub_field("body")): ?>
+	      <div class="section<?=$count;?>__rightText splitRight__text cell">
+	        <?php if (get_sub_field("header")): ?><h1><?=get_sub_field("header");?></h1><?php endif;?>
+        <?php if (get_sub_field("tagline")): ?><h2><?=get_sub_field("tagline");?></h2><?php endif;?>
+        <?php if (get_sub_field("body")): ?><p><?=get_sub_field("body");?></p><?php endif;?>
       </div>
-    <?php endif; ?>
-  <?php if ( get_sub_field("image")):?><img class="section<?=$count;?>__rightImage splitRight__image" src="<?= get_sub_field("image")["url"]; ?>"><?php endif;?>
-  <?php if ( have_rows( "right_buttons" ) ): $btn_count = 1; ?>
-  <div class="section<?=$count;?>__rightButtons splitRight__buttons splitRight__buttons--<?= $alignment; ?> cell">
-  <?php while ( have_rows( "right_buttons" ) ): the_row(); ?>
-    <a href="<?php the_sub_field( "link" ); ?>" class="section<?=$count;?>__rightButton section<?=$count;?>__rightButton<?=$btn_count;?> button splitRight__button splitRight<?=$count;?>__button<?=$btn_count;?>"><?php the_sub_field( "text" ); ?></a>
-  <?php $btn_count++; endwhile; ?>
-  </div>
-  <?php else: ?>
+    <?php endif;?>
+  <?php if (get_sub_field("fg_image")): ?><img class="section<?=$count;?>__rightImage splitRight__image" src="<?=get_sub_field("fg_image")["url"];?>"><?php endif;?>
+  <?php if (have_rows("right_buttons")): $btn_count = 1;?>
+	  <div class="section<?=$count;?>__rightButtons splitRight__buttons splitRight__buttons--<?=$alignment;?> cell">
+	  <?php while (have_rows("right_buttons")): the_row();?>
+		    <a href="<?php the_sub_field("link");?>" class="section<?=$count;?>__rightButton section<?=$count;?>__rightButton<?=$btn_count;?> button splitRight__button splitRight<?=$count;?>__button<?=$btn_count;?>"><?php the_sub_field("text");?></a>
+		  <?php $btn_count++;endwhile;?>
+	  </div>
+	  <?php else: ?>
     <?php // no buttons found ?>
-  <?php endif; ?>
+  <?php endif;?>
   </div>
-    <?php endwhile; ?>
+    <?php endwhile;?>
       </div>
   <?php endif; // End of right content ?>
-  </section>
+</section>
