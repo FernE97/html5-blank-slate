47302079a84b43f71145ce4e632aa675fa281803
Eric Fernandez <ferne97@gmail.com>
2020-06-11 18:27:49 -0700

    set version to 1.0.0 / updated author

--- functions.php
+++ functions.php
@@ -3,8 +3,8 @@
 // Enqueue Styles
 function h5bs_enqueue_styles() {
   wp_enqueue_style( 'font-awesome', 'https://use.fontawesome.com/releases/v5.4.2/css/all.css"' );
-  wp_enqueue_style( 'h5bs-theme', get_template_directory_uri() . '/assets/css/theme.css', false, '3.6.0' );
-  wp_enqueue_style( 'h5bs-custom', get_template_directory_uri() . '/custom.css', false, '3.6.0' );
+  wp_enqueue_style( 'h5bs-theme', get_template_directory_uri() . '/assets/css/theme.css', false, '1.0.0' );
+  wp_enqueue_style( 'h5bs-custom', get_template_directory_uri() . '/custom.css', false, '1.0.0' );
 }
 
 add_action( 'wp_enqueue_scripts', 'h5bs_enqueue_styles' );
@@ -12,9 +12,9 @@ add_action( 'wp_enqueue_scripts', 'h5bs_enqueue_styles' );
 
 // Enqueue Scripts
 function h5bs_enqueue_scripts() {
-  wp_enqueue_script('vendor-js', get_template_directory_uri() . '/assets/js/vendor.bundle.js', array('jquery'), '3.6.0', true);
-  // wp_enqueue_script('commons-js', get_template_directory_uri() . '/assets/js/commons.bundle.js', array(), '3.6.0', true);
-  wp_enqueue_script('bundle-js', get_template_directory_uri()  . '/assets/js/bundle.js', array('jquery'), '3.6.0', true);
+  wp_enqueue_script('vendor-js', get_template_directory_uri() . '/assets/js/vendor.bundle.js', array('jquery'), '1.0.0', true);
+  // wp_enqueue_script('commons-js', get_template_directory_uri() . '/assets/js/commons.bundle.js', array(), '1.0.0', true);
+  wp_enqueue_script('bundle-js', get_template_directory_uri()  . '/assets/js/bundle.js', array('jquery'), '1.0.0', true);
 }
 
 add_action( 'wp_enqueue_scripts', 'h5bs_enqueue_scripts' );
