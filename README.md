217a6d0f604a51897484dc0395216d41518bbebe
Eric Fernandez <ferne97@gmail.com>
2022-08-29 21:01:16 -0700

    update gulp to v4

--- README.md
+++ README.md
@@ -2,10 +2,22 @@
 
 ### Basic Setup
 
-Install nodejs and yarn:
-
 - [nodejs](https://nodejs.org/en/)
-- [yarn](https://yarnpkg.com/en/docs/install)
+
+#### NVM (Node Version Manager)
+
+NVM is not required but is recommended for installing different node versions for various projects.
+
+```bash
+# install / update nvm
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+
+# install node 16
+nvm install 16.17.0
+
+# use node 16
+nvm use 16
+```
 
 From the console, change directories into the WordPress Theme
 
@@ -16,16 +28,16 @@ cd {path}/wp-content/themes/{theme_name}
 Install gulp (one time)
 
 ```
-npm install -g gulp
+npm i -g gulp@latest
 ```
 
-Run `yarn` to get install the JS dependencies
+Run `npm i` to install the JS dependencies
 
 ```
-yarn
+npm i
 ```
 
-Run `gulp dist` once initially, then when it's time to deploy or when you add 
+Run `gulp dist` once initially, then when it's time to deploy or when you add
 files to the commons or vendor bundles (see advanced usage below).  This will
 clean the assets dirs and compile all the JS/CSS bundles.  For deploy, you then
 just copy the assets dir up to the server.
@@ -34,7 +46,7 @@ just copy the assets dir up to the server.
 gulp dist
 ```
 
-Run `gulp watch` to watch for changes to the apps main scss/js source files as you work. 
+Run `gulp watch` to watch for changes to the apps main scss/js source files as you work.
 This will compile any changes made to files js/css in `src/` to their bundles
 in `assets/`.  Note this only watches files in `src/`; if, for example, you install
 an npm module and add it to the vendor bundle (see advanced usage) you will need
@@ -46,21 +58,21 @@ gulp watch
 
 ### Browsersync
 
-As an alternative to `gulp watch`, there is a task to run a 
+As an alternative to `gulp watch`, there is a task to run a
 [browsersync](https://www.browsersync.io/) server locally, `gulp server`.  By default
-it binds to `localhost`.  This and other options are found in `gulpconfig.yml`.
+it binds to `localhost`.  This and other options are found in `gulpfile.js`.
 
 
 ### Advanced JS Pipeline Usage
 
 The JS pipeline allows for writing code using CommonJS/node or ES6 syntax, and
 because of browserify, allows you to use appropriate modules from the npm
-ecosystem in the JS bundle without any configuration outside of `yarn add {the module}`
+ecosystem in the JS bundle without any configuration outside of `npm i {the module}`
 and `require`'ing (CommonJS) or `import`'ing (ES6) the module in your code.
 
 This means for example you could, at the command line:
 
-  yarn add underscore
+  npm i underscore
 
 Then in a bundled JS file:
 
@@ -71,22 +83,22 @@ OR equivalently:
   var underscore = require('underscore');
 
 If the modules you include grow in number or are complex, you might find that your
-JS build time slows down.  Optionally, you can add modules to the commons bundle 
-such that they're compiled separately.  Continuing the exmaple above, you'd edit 
-`gulpconfig.yml` to make this happen, adding "underscore" to the commons modules 
+JS build time slows down.  Optionally, you can add modules to the commons bundle
+such that they're compiled separately.  Continuing the exmaple above, you'd edit
+`gulpfile.js` to make this happen, adding "underscore" to the commons modules
 array.
 
-If you need to add other vendor JS scripts which are *not* common required 
-modules, you'd edit `gulpconfig.yml` to add their full paths to the vendor paths 
-array.  By default this is how foundation.js is included.  Other examples would 
-be any self contained script that expects to be loaded via a `<script>` tag, 
+If you need to add other vendor JS scripts which are *not* common required
+modules, you'd edit `gulpfile.js` to add their full paths to the vendor paths
+array.  By default this is how foundation.js is included.  Other examples would
+be any self contained script that expects to be loaded via a `<script>` tag,
 e.g. jQuery plugins or polyfills/shims.
 
 
 ### Advanced CSS Pipeline Usage
 
 The CSS pipeline is less complex than the JS, but there are still some options to
-be configured in `gulpconfig.yml`.  The full options objects passed to autoprefixer 
-and sass are found here.  A common change might be the `browsers` targeted by the 
-autoprefixer.  By default it is `latest 2 versions`.  See other examples 
+be configured in `gulpfile.js`.  The full options objects passed to autoprefixer
+and sass are found here.  A common change might be the `browsers` targeted by the
+autoprefixer.  By default it is `latest 2 versions`.  See other examples
 [here](https://github.com/ai/browserslist#queries).
--- gulpfile.js
+++ gulpfile.js
@@ -1,150 +1,188 @@
-var gulp = require('gulp'),
-  autoprefixer = require('gulp-autoprefixer'),
-  babel = require('gulp-babel'),
-  babelify = require('babelify'),
-  browserSync = require('browser-sync').create(),
-  browserify = require('browserify'),
-  buffer = require('vinyl-buffer'),
-  concat = require('gulp-concat'),
-  del = require('del'),
-  fs = require('fs'),
-  gutil = require('gulp-util'),
-  nano = require('gulp-cssnano'),
-  path = require('path'),
-  rename = require('gulp-rename'),
-  sass = require('gulp-sass'),
-  sassGlob = require('gulp-sass-glob'),
-  source = require('vinyl-source-stream'),
-  sourcemaps = require('gulp-sourcemaps'),
-  uglify = require('gulp-uglify'),
-  yaml = require('js-yaml')
+const gulp = require('gulp')
+const autoprefixer = require('gulp-autoprefixer')
+const babel = require('gulp-babel')
+const babelify = require('babelify')
+const browserSync = require('browser-sync').create()
+const browserify = require('browserify')
+const buffer = require('vinyl-buffer')
+const concat = require('gulp-concat')
+const del = require('del')
+const gutil = require('gulp-util')
+const nano = require('gulp-cssnano')
+const path = require('path')
+const sass = require('gulp-sass')(require('node-sass'))
+const sassGlob = require('gulp-sass-glob')
+const source = require('vinyl-source-stream')
+const sourcemaps = require('gulp-sourcemaps')
+const uglify = require('gulp-uglify-es').default
+const cache = require('gulp-cache')
 
-var config = yaml.safeLoad(fs.readFileSync('./gulpconfig.yml', 'utf8'));
+const config = {
+  js: {
+    dest: './assets/js',
+    vendor: {
+      paths: [
+        './node_modules/foundation-sites/dist/js/foundation.js',
+      ],
+      filename: 'vendor.bundle.js'
+    },
+    commons: {
+      modules: [],
+      filename: 'commons.bundle.js'
+    },
+    bundle: {
+      entries: './src/js/bundle.js',
+      filename: 'bundle.js'
+    }
+  },
+  css: {
+    dest: './assets/css',
+    sass: {
+      src: './src/scss/theme.scss',
+      includePaths: [
+        './node_modules',
+        './node_modules/foundation-sites/scss',
+      ]
+    },
+    autoprefixer: {
+      cascase: false
+    }
+  },
+  browsersync: {
+    proxy: 'localhost',
+    notify: false,
+    open: false,
+    snippetOptions: { ignorePaths: 'wp-admin/**' }
+  }
+}
 
-
-// Concat/minify self-contained scripts or scripts that aren't otherwise used
-// as CommonJS type modules.  By default this includes the Foundation script.
-// Other possibilities would be polyfill/shim scripts, etc.
-gulp.task('js-vendor', function () {
-  return gulp.src(config.js.vendor.paths)
+async function jsVendor() {
+  return gulp
+    .src(config.js.vendor.paths)
     .pipe(concat(config.js.vendor.filename))
     .pipe(babel())
     .pipe(uglify())
-    .pipe(gulp.dest(config.js.dest));
-});
+    .pipe(gulp.dest(config.js.dest))
+}
 
+exports.jsVendor = jsVendor
 
 // Optionally compile a separate browserify "commons" bundle of js that the
 // site's bundle can `require` from.  If you want to do this add node modules
-// to the js.commons.modules array in the yml and uncomment the enqueue for this
+// to the js.commons.modules array in the config and uncomment the enqueue for this
 // file in functions.php so that WP sends it.
 //
 // The main advantage is for dev, to reduce the compile time of the bundle
 // task, as if the modules are pulled to a commons bundle, they don't have to
 // be recompiled when the app bundle changes.
-gulp.task('js-commons', function () {
+async function jsCommons() {
   // See manual for using browserify with gulp/transforms:
   // https://github.com/gulpjs/gulp/blob/master/docs/recipes/browserify-transforms.md
 
-  var b = browserify({
+  const b = browserify({
     debug: true,
     transform: [babelify]
-  });
+  })
 
   if (!(config.js.commons.modules && config.js.commons.modules.length)) {
-    return;
+    return
   }
 
-  b.require(config.js.commons.modules);
+  b.require(config.js.commons.modules)
 
-  return b.bundle()
+  b.bundle()
     .pipe(source(config.js.commons.filename))
     .pipe(buffer())
     .pipe(uglify())
-      .on('error', gutil.log)
-    .pipe(gulp.dest(config.js.dest));
-});
-
+    .on('error', gutil.log)
+    .pipe(gulp.dest(config.js.dest))
+}
+exports.jsCommons = jsCommons
 
 // Bundle, sourcemap and minify the main app js
-gulp.task('js-bundle', function () {
+async function jsBundle() {
   // See manual for using browserify with gulp/transforms:
   // https://github.com/gulpjs/gulp/blob/master/docs/recipes/browserify-transforms.md
 
-  var b = browserify({
+  const b = browserify({
     entries: config.js.bundle.entries,
     debug: true,
-    transform: [
-      babelify
-    ]
-  });
+    transform: [['babelify', { presets: ['@babel/preset-env'] }]]
+  })
 
   if (config.js.commons.modules && config.js.commons.modules.length) {
-    b.external(config.js.commons.modules);
+    b.external(config.js.commons.modules)
   }
 
-  return b.bundle()
+  b.bundle()
     .pipe(source(config.js.bundle.filename))
     .pipe(buffer())
-    .pipe(sourcemaps.init({loadMaps: true}))
+    .pipe(sourcemaps.init({ loadMaps: true }))
     .pipe(uglify())
-      .on('error', gutil.log)
-    .pipe(sourcemaps.write('.'))
-    .pipe(gulp.dest(config.js.dest));
-});
+    .on('error', gutil.log)
+    .pipe(sourcemaps.write('./'))
+    .pipe(gulp.dest(config.js.dest))
+}
 
+exports.jsBundle = jsBundle
 
 // Compile scss/sass files into minified, sourcemapped, autoprefixed CSS
-gulp.task('sass', function () {
-  return gulp.src(config.css.sass.src)
+async function sassBundle(cb) {
+  return gulp
+    .src(config.css.sass.src)
     .pipe(sourcemaps.init())
     .pipe(sassGlob())
     .pipe(sass(config.css.sass))
     .pipe(autoprefixer(config.css.autoprefixer))
     .pipe(sass.sync().on('error', sass.logError))
-    .pipe(nano())
+    .pipe(cache(nano()))
     .pipe(sourcemaps.write('.'))
-    .pipe(gulp.dest(config.css.dest));
-});
+    .pipe(gulp.dest(config.css.dest))
+}
 
+exports.sassBuild = sassBundle
 
 // Watch the source folders for changes and run compile tasks.  This task should
 // always be running during development for automatic compilation of assets.
-gulp.task('watch', function () {
-  gulp.watch('src/scss/**/*.scss', ['sass']);
-  gulp.watch('src/js/**/*.js', ['js-bundle']);
-});
+function watch() {
+  gulp.watch('src/scss/**/*.scss', gulp.series(cssBuild))
+  gulp.watch('src/js/**/*.js', gulp.series(jsBuild))
+}
 
-
-// Run a browsersync server, main options managed in gulpconfig.yml
-// Alternative to `gulp watch`, run this if you want to make use of browsersync
-// for real time code/css updates, etc.
-gulp.task('server', ['watch'], function () {
-  browserSync.init(config.browsersync);
-
-  browserSync.watch([
-    'assets/css/theme.css',
-    'assets/js/bundle.js',
-    '**/*.php'
-  ])
-  .on('change', browserSync.reload)
-  .on('error', function (err) {
-    console.log('error in browsersync watch', err);
-    this.emit('end');
-  });
-});
-
+exports.watch = watch
 
 // Clean gulp-generated js/css assets (automatically run as part of dist)
-gulp.task('clean', function () {
-  return del([
-    path.join(config.js.dest, '*'),
-    path.join(config.css.dest, '*')
-  ]);
-});
+function clean() {
+  return del([path.join(config.js.dest, '*'), path.join(config.css.dest, '*')])
+}
 
+exports.clean = clean
 
-// Multistep tasks & default
-gulp.task('js', ['js-vendor', 'js-commons', 'js-bundle']);
-gulp.task('dist', ['clean', 'js', 'sass']);
-gulp.task('default', ['watch']);
+async function jsBuild() {
+  return gulp.parallel(jsVendor, jsCommons, jsBundle)()
+}
+async function cssBuild() {
+  return gulp.parallel(sassBundle)()
+}
+async function dist() {
+  return gulp.series(clean, jsBuild, sassBundle)()
+}
+
+function server() {
+  return gulp.parallel(watch, () => {
+    browserSync.init(config.browsersync)
+
+    browserSync
+      .watch(['assets/css/theme.css', 'assets/js/bundle.js', '**/*.php'])
+      .on('change', browserSync.reload)
+      .on('error', function (err) {
+        console.log('error in browsersync watch', err)
+        this.emit('end')
+      })
+  })()
+}
+exports.server = server
+
+exports.js = jsBuild
+exports.sass = cssBuild
+exports.dist = dist
